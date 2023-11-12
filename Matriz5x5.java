16ff25bba1374895fa5c76175c499f4307d516f1
IsaacNietoG <isaaniet@gmail.com>
2023-11-11 20:18:22 -0600

    feat: Termina el metodo multipMatrices de la clase Matriz5x5

--- Matriz5x5.java
+++ Matriz5x5.java
@@ -1,20 +1,36 @@
 import java.util.Random;
 public class Matriz5x5 {
     protected Random randomizador = new Random();
-    protected int[][] matriz = new int[5][5];
+    int[][] matriz = new int[5][5];
 
-    public Matriz5x5(){
+    public Matriz5x5(int aux){
         /**
-           Genera un objeto Matriz5x5 con numeros entre -9 y 9
+           Genera un objeto Matriz5x5, ya sea vacio o aleatorizado.
 
+           Este metodo constructor tiene dos modos que se pueden escoger segun
+           el argumento que se le de al constructor. Uno generará una instancia
+           cuyo arreglo interno será lleno de números aleatorios en un rango de -9
+           a 9, el otro modo generará solamente la instancia con su arreglo interno
+           vacío.
+           Se implementaron dos modos para evitar uso innecesario de recursos, pues
+           para los métodos estáticos de esta clase que retornan una instancia de
+           la misma, sería ineficiente generar una matriz aleatorizada que luego
+           se reescribirá.
            Utiliza la clase Random de java para rellenar el arreglo interno de la
            instancia de Matriz5x5
 
+           @param aux    Si el int proporcionado como argumento es distinto a 0,
+                         se construirá una instancia cuyo arreglo interno contendrá
+                         números aleatorizados en un rango de -9 a 9, caso opuesto,
+                         se generará una instancia cuyo arreglo interno esté vacío.
+
            @return una instancia de Matriz5x5
          */
-        for(int i = 0; i<matriz.length; i++){
-            for(int j = 0; j<matriz[i].length; j++){
-                matriz[i][j] = randomizador.nextInt(19)-9;
+        if(aux != 0){
+            for(int i = 0; i<matriz.length; i++){
+                for(int j = 0; j<matriz[i].length; j++){
+                    matriz[i][j] = randomizador.nextInt(19)-9;
+                }
             }
         }
     }
@@ -29,7 +45,43 @@ public class Matriz5x5 {
         matriz = input;
     }
 
-    
+    public static Matriz5x5 sumaMatrices(Matriz5x5 sumando1, Matriz5x5 sumando2){
+        /**
+           Suma dos matrices de 5x5.
 
+           @param sumando1 & sumando2  Las instancias que se usarán como sumando
+
+           @return una Matriz5x5 que será el resultado de la suma.
+         */
+        Matriz5x5 resultado = new Matriz5x5(0);
+        for(int i = 0; i<sumando1.matriz.length; i++){
+            for(int j = 0; j<sumando1.matriz.length; j++){
+                resultado.matriz[i][j] = sumando1.matriz[i][j] + sumando2.matriz[i][j];
+            }
+        }
+        return resultado;
+    }
+
+    public static Matriz5x5 multipMatrices(Matriz5x5 factor1, Matriz5x5 factor2){
+        /**
+           Multiplica dos matrices de 5x5.
+
+           @param factor1 & factor2   Las instancias que se usarán como factores
+           de la operación.
+
+           @return una Matriz5x5 que será el resultado de multiplicar ambos params
+         */
+        Matriz5x5 resultado = new Matriz5x5(0);
+        for(int i = 0; i<factor1.matriz.length; i++){
+            for(int j = 0; j<factor1.matriz.length; j++){
+                int subresultado=0;
+                for(int k = 0; k<factor1.matriz.length; k++){
+                    subresultado += factor1.matriz[k][j] * factor2.matriz[i][k];
+                }
+                resultado.matriz[i][j] = subresultado;
+            }
+        }
+        return resultado;
+    }
 
 }
